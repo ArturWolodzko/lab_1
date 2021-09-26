55f978d6b67b2ed50222f10227327a16f4e4c557
notoja12 <notoja12@gmail.com>
2021-09-26 12:28:01 +0200

    start programu

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+        System.out.println("to moj pierwszy tekst napisany w java");
+        System.out.printf("I %s  potrafie obslugiwać tego programu \n","nie");
 
 
     }
