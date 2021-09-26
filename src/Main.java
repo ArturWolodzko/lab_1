d55c511b242e240fd0d332c0a80d06e75051f217
notoja12 <notoja12@gmail.com>
2021-09-26 12:33:24 +0200

    start programu

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,14 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
         System.out.println("to moj pierwszy tekst napisany w java");
-        System.out.printf("I %s  potrafie obslugiwać tego programu \n","nie");
+
+
+        System.out.printf("I %s  potrafie obslugiwać %s programu \n","nie" , "tego");
+        System.out.printf("to są nasze %d zajęcia \n", 1);
+
+        System.out.print("Ta funkcja słuzy");
+        System.out.print("do formatowania baaaaaaaaaaardzo");
+        System.out.print("długich tekstów");
 
 
     }
