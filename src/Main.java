99e2eda386a93088096ef6c16d969a97b39654e5
notoja12 <notoja12@gmail.com>
2021-09-26 13:03:07 +0200

    start programu

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -6,6 +7,10 @@ public class Main {
 
         int zmiennaInt = 7;
         System.out.println("zmienna int = " + zmiennaInt);
+        float zmiennaFloat = 2.2124f;
+        System.out.println("zmiennaFloat =" + zmiennaFloat);
+        System.out.printf("zmiennaFloat = %f" , zmiennaFloat );
+        System.out.println("zmiennaInt = " + zmiennaInt);
         /* ćw.2 */
 
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
@@ -16,8 +21,24 @@ public class Main {
         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
         *  */
-
-
+       /* int a=6 , b=3, ;
+        int wynik = a + b;
+        int wynik2 = a - b;
+        int wynik3 = a * b;
+        int wynik4 = a / b;
+                System.out.println("wynik dodawania: "+wynik);
+        System.out.println("wynik odejmowania :"+wynik2);
+        System.out.println("wynik mnożenia:"+wynik3);
+       System.out.println("wynik dzielenia:"+wynik4);*/
+        Double x=2.45 , y=3.56;
+        double zmienna = x + y;
+        double zmienna1 = x - y;
+        double zmienna2 = x * y;
+        double zmienna3 = x / y;
+        System.out.println("wynik dodawania wynosi:"+zmienna);
+        System.out.println("wynik odejmowania wynosi:"+zmienna1);
+        System.out.println("wynik mnożenia wynosi"+zmienna2);
+        System.out.println("wynik dzielenia wynosi"+zmienna3);
 
     }
 }
