56d1dc0e5bac16b34cf33440b048ef7628602a2d
ArturWolodzko <notoja12@gmail.com>
2021-10-15 08:02:12 +0200

    start programu

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
         System.out.println("a == b daje wynik:  "   + (a == b ));
         System.out.println("a + b daje wynik:  " + (a + b));
-        System.out.println("a && b daje wynik:  " + (a && b));
+        System.out.println("a & b daje wynik:  " + (a & b));
 
 
     }
