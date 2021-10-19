bed4e99f64b37a1b8e751ac9668dc16877c68733
ArturWolodzko <notoja12@gmail.com>
2021-10-19 10:42:57 +0200

    start programu

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,19 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
         System.out.println("to moj pierwszy tekst napisany w java");
+        System.out.println("przypomnienie komend po 2 tygodniach");
 
 
         System.out.printf("I %s  potrafie obslugiwać %s programu \n","nie" , "tego");
         System.out.printf("to są nasze %d zajęcia \n", 1);
+        System.out.printf("bo łatwo sie je zapomina a zwłaszcza po %d tygodniach", 3);
+        System.out.printf(" już jestem na %d cwiczeniu z %s zajęc \n" , 6 , "pierwszych");
 
         System.out.print("Ta funkcja słuzy");
         System.out.print("do formatowania baaaaaaaaaaardzo");
-        System.out.print("długich tekstów");
+        System.out.print("długich tekstów \n");
+        System.out.print("zwykły print służy do ");
+        System.out.println("pisania tekstów w jednej lini");
 
 
     }
