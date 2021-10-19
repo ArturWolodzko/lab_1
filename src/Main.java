de0a6f133d0d110b5992fe703330ff2a36c6a33b
ArturWolodzko <notoja12@gmail.com>
2021-10-19 17:30:17 +0200

    start programu

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,9 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
-        String[] tab1 = {mercedes,audi,bmw,vw}
+        double[] tab1 = {10f, 20f, 30f, 40f, 50f};
+
+        System.out.println(tab1);
 
 
 
